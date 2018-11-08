fdc2d0e53b81cc0e191c44370a46ee7e8357bdcb
Andrés Reynoso Mazoy <andres.reynoso.mazoy@gmail.com>
2018-11-07 18:30:07 -0600

    Add writing to files and image downloading

--- scraper.py
+++ scraper.py
@@ -1,4 +1,6 @@
 import re
+import argparse
+import pathlib
 from itertools import takewhile
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
@@ -38,22 +40,36 @@ def get_image_uris(markup):
     '''
     Finds all the post's image uris, extracted from the JS code.
     '''
-    uri_list = re.findall('static.wixstatic.com[^"&]*.jpg', markup)
+    uri_list = re.findall('static.wixstatic.com[^"&]*.jp[e]?g', markup)
     # Place into a set to make unique.
     uri_set = set(uri_list)
     # Remove escape slashes.
     return [re.sub(r'\\', '', uri) for uri in uri_set]
 
+ap = argparse.ArgumentParser()
+ap.add_argument('-u', '--url', required=True, help='URL to the post')
+ap.add_argument('-o', '--output', required=True, help='Path of output folder to be created')
+args = vars(ap.parse_args())
 
 try:
-    html = urlopen('http://clarisseandandres.wixsite.com/wanderlust/single-post/2017/10/14/Ehrwald')
+    html = urlopen(args['url'])
     html_content = html.read()
 except HTTPError as e:
     print(e)
 else:    
     bs = BeautifulSoup(html_content, 'html.parser')
-    print(get_title(bs))
-    print("Written by: {}".format(get_author(bs)))
-    print(get_main(bs))
-    for uri in get_image_uris(html_content.decode('utf-8')):
-        print(uri)
+    pathlib.Path(args['output'] + '/images').mkdir(parents=True, exist_ok=True)
+    with open(args['output'] + '/post.txt', 'w+') as f:
+        f.write(get_title(bs))
+        f.write('\n')
+        f.write("Written by {}".format(get_author(bs)))
+        f.write('\n\n')
+        f.write(get_main(bs))
+
+    # Download and save images.
+    image_uris = get_image_uris(html_content.decode('utf-8'))
+    for i, uri in enumerate(image_uris):
+        if 'mv2' in uri:
+            with open(args['output'] + '/images/' + str(i) + '.jpg', 'wb+') as f:
+                f.write(urlopen('http://' + uri).read())
+
